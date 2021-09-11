280c832fd61d3e185b26f7807e37cea8a273dfb7
janisroze <janis.roze1@gmail.com>
2021-09-11 17:41:44 +0300

    removal of unneeded code

--- src/com/jr/vehiclerental/Main.java
+++ src/com/jr/vehiclerental/Main.java
@@ -5,12 +5,6 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
-        System.out.println("testin output");
-        try {
-            System.in.read();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
     }
 }
